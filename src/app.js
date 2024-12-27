f38bc18f308981b0c0716924496a3710cc43493f
Dylan <mccarteydylan@gmail.com>
2024-12-27 16:22:32 -0600

    set up migrations

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
+
+const notFound = require('./errors/notFound');
+const errorHandler = require('./errors/errorHandler');
+
+const moviesRouter = require('./movies/movies.router');
+const reviewsRouter = require('./reviews/reviews.router');
+const theatersRouter = require('./theaters/theaters.router');
+
 const app = express();
+
+app.use(cors()); //allows cors
+app.use(express.json()); //allows JSON parse
+//Routes
+app.use('/movies', moviesRouter);
+app.use('/reviews', reviewsRouter);
+app.use('/theaters', theatersRouter);
+//Errors
+app.use(notFound); //No path
+app.use(errorHandler); //General
+
 
 module.exports = app;
